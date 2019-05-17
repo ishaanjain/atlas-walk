cd617f76ed8007a98762a54f1b2cd42b2adce94d
Joulien <joulien_i@yahoo.com>
2019-05-16 21:05:22 -0700

    changed actor network training
    project builds. training occurs after 50 episodes (?)

--- DDPG/src/actor_network.py
+++ DDPG/src/actor_network.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import math
+import src.ops as ops
+
 
 # Hyper Parameters
 LAYER1_SIZE = 400
@@ -16,11 +18,11 @@ class ActorNetwork:
         self.state_dim = state_dim
         self.action_dim = action_dim
         # create actor network
-        self.state_input, self.action_output, self.net = self.create_network(state_dim, action_dim)
+        self.state_input, self.action_output, self.net = self.create_network(state_dim, action_dim, self.args.train)
 
         # create target actor network
         self.target_state_input, self.target_action_output, self.target_update, self.target_net = self.create_target_network(
-            state_dim, action_dim, self.net)
+            state_dim, action_dim, self.args.train, self.net)
 
         # define training rules
         self.create_training_method()
@@ -36,36 +38,68 @@ class ActorNetwork:
         self.parameters_gradients = tf.gradients(self.action_output, self.net, -self.q_gradient_input)
         self.optimizer = tf.train.AdamOptimizer(self.args.actor_learning_rate).apply_gradients(zip(self.parameters_gradients, self.net))
 
-    def create_network(self, state_dim, action_dim):
-        layer1_size = LAYER1_SIZE
-        layer2_size = LAYER2_SIZE
+    def create_network(self, state_dim, action_dim, is_train):
+        with tf.variable_scope("actor", reuse=tf.AUTO_REUSE):
+            state_input = tf.placeholder("float", [None, state_dim])
 
-        state_input = tf.placeholder("float", [None, state_dim])
+            # W1 = self.variable([state_dim, layer1_size], state_dim)
+            # b1 = self.variable([layer1_size], state_dim)
+            # W2 = self.variable([layer1_size, layer2_size], layer1_size)
+            # b2 = self.variable([layer2_size], layer1_size)
+            # W3 = tf.Variable(tf.random_uniform([layer2_size, action_dim], -3e-3, 3e-3))
+            # b3 = tf.Variable(tf.random_uniform([action_dim], -3e-3, 3e-3))
 
-        W1 = self.variable([state_dim, layer1_size], state_dim)
-        b1 = self.variable([layer1_size], state_dim)
-        W2 = self.variable([layer1_size, layer2_size], layer1_size)
-        b2 = self.variable([layer2_size], layer1_size)
-        W3 = tf.Variable(tf.random_uniform([layer2_size, action_dim], -3e-3, 3e-3))
-        b3 = tf.Variable(tf.random_uniform([action_dim], -3e-3, 3e-3))
+            # layer1 = tf.nn.relu(tf.matmul(state_input, W1) + b1)
+            # layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2)
+            # action_output = tf.tanh(tf.matmul(layer2, W3) + b3)
 
-        layer1 = tf.nn.relu(tf.matmul(state_input, W1) + b1)
-        layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2)
-        action_output = tf.tanh(tf.matmul(layer2, W3) + b3)
+            W1 = tf.glorot_uniform_initializer()
+            b1 = tf.constant_initializer(0.0)
+            W2 = tf.glorot_uniform_initializer()
+            b2 = tf.constant_initializer(0.0)
+            W3 = tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3)
+            b3 = tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3)
 
-        return state_input, action_output, [W1, b1, W2, b2, W3, b3]
+            layer1 = ops.fully_connected(state_input, LAYER1_SIZE, w_init=W1, b_init=b1, is_training=is_train, scope="layer1")
+            layer1_relu = ops.activation_function(layer1, scope="layer1_relu")
+            layer2 = ops.fully_connected(layer1_relu, LAYER2_SIZE, w_init=W2, b_init=b2, is_training=is_train, scope="layer2")
+            layer2_relu = ops.activation_function(layer2, scope="layer2_relu")
+            layer3 = ops.fully_connected(layer2_relu, action_dim, w_init=W3, b_init=b3, is_training=is_train, scope="layer3")
+            action_output = ops.activation_function(layer3, scope="action_output", activation=tf.nn.tanh)
 
-    def create_target_network(self, state_dim, action_dim, net):
-        state_input = tf.placeholder("float", [None, state_dim])
-        ema = tf.train.ExponentialMovingAverage(decay=1 - self.args.tau)
-        target_update = ema.apply(net)
-        target_net = [ema.average(x) for x in net]
+        variables = tf.contrib.framework.get_variables(scope="actor")
+        print(variables)
 
-        layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
-        layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + target_net[3])
-        action_output = tf.tanh(tf.matmul(layer2, target_net[4]) + target_net[5])
+        return state_input, action_output, variables
+
+    def create_target_network(self, state_dim, action_dim, is_train, net):
+        with tf.variable_scope("actor", reuse=tf.AUTO_REUSE):
+            state_input = tf.placeholder("float", [None, state_dim])
+            ema = tf.train.ExponentialMovingAverage(decay=1 - self.args.tau)
+            target_update = ema.apply(net)
+            target_net = [ema.average(x) for x in net]
+
+            # layer1 = ops.fully_connected(state_input, LAYER1_SIZE, w_init=W1, b_init=b1, is_training=is_train, scope="layer1")
+            layer1_relu = ops.activation_function(tf.matmul(state_input, target_net[0]) + target_net[1], scope="layer1_relu")
+            # layer2 = ops.fully_connected(layer1_relu, LAYER2_SIZE, w_init=W2, b_init=b2, is_training=is_train, scope="layer2")
+            layer2_relu = ops.activation_function(tf.matmul(layer1_relu, target_net[6]) + target_net[7], scope="layer2_relu")
+            # layer3 = ops.fully_connected(layer2_relu, action_dim, w_init=W3, b_init=b3, is_training=is_train, scope="layer3")
+            action_output = ops.activation_function(tf.matmul(layer2_relu, target_net[12]) + target_net[13], scope="action_output", activation=tf.nn.tanh)
+
+        # variables = tf.contrib.framework.get_variables(scope="actor")
 
         return state_input, action_output, target_update, target_net
+
+        # state_input = tf.placeholder("float", [None, state_dim])
+        # ema = tf.train.ExponentialMovingAverage(decay=1 - self.args.tau)
+        # target_update = ema.apply(net)
+        # target_net = [ema.average(x) for x in net]
+        #
+        # layer1 = tf.nn.relu(tf.matmul(state_input, target_net[0]) + target_net[1])
+        # layer2 = tf.nn.relu(tf.matmul(layer1, target_net[2]) + target_net[3])
+        # action_output = tf.tanh(tf.matmul(layer2, target_net[4]) + target_net[5])
+        #
+        # return state_input, action_output, target_update, target_net
 
     def update_target(self):
         self.sess.run(self.target_update)
@@ -75,6 +109,8 @@ class ActorNetwork:
             self.q_gradient_input: q_gradient_batch,
             self.state_input: state_batch
         })
+        # print(self.sess.run([self.net[0], self.net[6], self.net[12]]))
+
 
     def actions(self, state_batch):
         return self.sess.run(self.action_output, feed_dict={
--- DDPG/src/ops.py
+++ DDPG/src/ops.py
@@ -3,17 +3,19 @@ import tensorflow as tf
 def fully_connected(inputs,
                     output_size,
                     w_init=tf.glorot_uniform_initializer(),
+                    b_init=tf.constant_initializer(0.0),
                     batch_norm=True,
                     is_training=True,
                     scope=None):
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
-        input_size = inputs.get_shape().as_list()[0]
+        # input_size = inputs.get_shape().as_list()[0]
+        input_size = inputs.shape[1]
 
         weights = tf.get_variable('weights', shape=[input_size, output_size],
                                   dtype=tf.float32, initializer=w_init)
 
         biases = tf.get_variable('biases', shape=[output_size], dtype=tf.float32,
-                                 initializer=tf.constant_initializer(0.0))
+                                 initializer=b_init)
 
         dense = tf.matmul(inputs, weights) + biases
 
